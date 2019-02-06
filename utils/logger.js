7b2c13266303b94fbb6c111a5f9441f7adf71f76
FalseData <xpyxel@gmail.com>
2019-02-06 08:34:28 -0800

    Add sentry.io support to logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,5 @@
 const Chalk = require("chalk");
+const Sentry = require("@sentry/node");
 
 class Logger
 {
@@ -9,6 +10,9 @@ class Logger
         this.ERROR = 1;
         this.WARNING = 2;
         this.DEBUG = 3;
+        if(config.sentry.enabled) Sentry.init({
+            dsn: config.sentry.dsn
+        });
     }
     log(logType = 0, message = "")
     {
@@ -16,15 +20,19 @@ class Logger
         switch(logType)
         {
             case 0:
+                if(this.config.sentry.enabled) Sentry.captureMessage(message, Sentry.Severity.Info);
                 console.log(time + Chalk.green(" info -> ") + message);
                 break;
             case 1:
+                if(this.config.sentry.enabled) Sentry.captureMessage(message, Sentry.Severity.Error);
                 console.log(time + Chalk.red(" erro -> ") + message);
                 break;
             case 2:
+                if(this.config.sentry.enabled) Sentry.captureMessage(message, Sentry.Severity.Warning);
                 console.log(time + Chalk.yellow(" warn -> ") + message);
                 break;
             case 3:
+                if(this.config.sentry.enabled) Sentry.captureMessage(message, Sentry.Severity.Debug);
                 console.log(time + Chalk.blue(" debg -> ") + message);
                 break;
         }
