d9ac03cc2d1a264fe15a6d32b82aea36baff2ee5
FalseData <xpyxel@gmail.com>
2019-02-06 07:21:13 -0800

    Setup basic connection log

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const Discord = require("discord.js");
+const LoggerUtil = require("./utils/logger");
 const Yaml = require("node-yaml");
 
 const Config = Yaml.readSync("config.yml");
+const Logger = new LoggerUtil(Config);
 const Client = new Discord.Client();
 
-Client.on("ready", async () => {});
+Client.on("ready", async () => {
+    Logger.log(Logger.INFO, "Connected to discord");
+});
 
 Client.on("message", async (msg) => {});
 
