c36c1727d3874b5091daa66a6bf45a79f6a847f1
FalseData <xpyxel@gmail.com>
2019-02-06 23:47:43 -0800

    Check if the plugin exists before reloading

--- plugins/reload.js
+++ plugins/reload.js
@@ -4,12 +4,17 @@ module.exports = {
     usage: "reload <module>",
     script: function(args)
     {
-        try {
-            args.plugins.reload(args.tokens[1]);
-            args.message.channel.send(`Reloaded plugin \`${args.tokens[1]}\` successfully`);
-        } catch(err) {
-            let reason = "```js\n" + err.stack + "```";
-            args.message.channel.send(`The plugin \`${args.tokens[1]}\` failed to reload ${reason}`);
+        if(args.plugins.plugins[args.tokens[1]])
+        {
+            try {
+                args.plugins.reload(args.tokens[1]);
+                args.message.channel.send(`Reloaded plugin \`${args.tokens[1]}\` successfully`);
+            } catch(err) {
+                let reason = "```js\n" + err.stack + "```";
+                args.message.channel.send(`The plugin \`${args.tokens[1]}\` failed to reload ${reason}`);
+            }
+        } else {
+            args.message.channel.send(`The plugin \`${args.tokens[1]}\` does not exist`);
         }
     }
 };
