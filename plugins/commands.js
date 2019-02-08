52f01f50b1189a1d7933485c9e4723a8bd53f1f6
FalseData <xpyxel@gmail.com>
2019-02-07 23:17:40 -0800

    Add a plugin to list available commands

---
+++ plugins/commands.js
@@ -0,0 +1,12 @@
+module.exports = {
+    permission: 0,
+    description: "Returns a list of available bot commands",
+    usage: "commands",
+    script: function(args)
+    {
+        let commandList = [];
+        let commands = args.plugins.plugins;
+        for(let cmd in commands) commandList.push(commands[cmd].name);
+        args.message.channel.send(`Available Commands: \`${commandList.join(", ")}\``);
+    }
+};
