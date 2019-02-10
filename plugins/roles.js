eb0e83d076c8b584420f971f1d0a46e049f8085a
FalseData <xpyxel@gmail.com>
2019-02-10 13:35:16 -0800

    create a roles command

---
+++ plugins/roles.js
@@ -0,0 +1,17 @@
+module.exports = {
+    permission: 1,
+    description: "Lists all the roles in the guild with ids",
+    usage: "roles",
+    script: function(args)
+    {
+        let roleStr = [];
+        let roles = args.message.guild.roles.array();
+        roles.splice(0, 1);
+        for(let role in roles)
+        {
+            let r = roles[role];
+            roleStr.push(r.name + " - " + r.id);
+        }
+        args.message.channel.send("```\n" + roleStr.join("\n") + "```\n");
+    }
+};
