161ed20aa16f5ae43088e4dc5ab57ea33f211643
FalseData <xpyxel@gmail.com>
2019-02-11 14:34:39 -0800

    Update help so it shows commands if no params given

--- plugins/help.js
+++ plugins/help.js
@@ -18,7 +18,16 @@ module.exports = {
                 permissionLevel
             ]));
         } else if(!args.tokens[1]) {
-            args.message.channel.send(args.translator.translate("noplugin", []));
+            let commandList = [];
+            let commands = args.plugins.plugins;
+            for(let cmd in commands)
+            {
+                if(commands[cmd].permission <= args.permission)
+                {
+                    commandList.push(`${commands[cmd].name} - ${commands[cmd].description}`);
+                }
+            }
+            args.message.channel.send(args.translator.translate("commands", [commandList.join("\n")]));
         } else {
             args.message.channel.send(args.translator.translate("invalidplugin", [args.tokens[1]]));
         }
