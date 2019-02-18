e20b747c9670f6949baf0f178a0a5df71fad6bb1
falsedata <xpyxel@gmail.com>
2019-02-18 10:36:07 -0500

    Add guild logs for modifying tags

--- plugins/tag.js
+++ plugins/tag.js
@@ -19,11 +19,21 @@ module.exports = {
                 tags[args.tokens[2]] = tagContent;
                 args.database.collection("Guilds").findOneAndUpdate({ id: args.message.guild.id }, { "$set": { tags: tags } }, { upsert: false });
                 args.message.channel.send(args.translator.translate("tagadded", [args.tokens[2]]));
+                args.logger.push(guild, args.translator.translate("gtagremoved", [
+                    args.tokens[2], 
+                    args.message.author.tag, 
+                    args.message.author.id
+                ]));
                 break;
             case "remove":
                 delete tags[args.tokens[2]];
                 args.database.collection("Guilds").findOneAndUpdate({ id: args.message.guild.id }, { "$set": { tags: tags } }, { upsert: false });
                 args.message.channel.send(args.translator.translate("tagremoved", [args.tokens[2]]));
+                args.logger.push(guild, args.translator.translate("gtagremoved", [
+                    args.tokens[2], 
+                    args.message.author.tag, 
+                    args.message.author.id
+                ]));
                 break;
             default:
                 if(tags[args.tokens[1]]) args.message.channel.send(tags[args.tokens[1]]);
